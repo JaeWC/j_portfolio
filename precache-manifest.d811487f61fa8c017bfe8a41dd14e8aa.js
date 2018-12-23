a359dea8b0bd62103b831c38ee48c03957f5efd1
Jae Choi <j.thechois@gmail.com>
2018-12-24 00:07:51 +0900

    Updates

---
+++ precache-manifest.d811487f61fa8c017bfe8a41dd14e8aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5d20130017c4515f505",
+    "url": "/j_portfolio/static/js/runtime~main.b5d20130.js"
+  },
+  {
+    "revision": "fc7734f7a047c1b7f70f",
+    "url": "/j_portfolio/static/js/main.fc7734f7.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/j_portfolio/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "fc7734f7a047c1b7f70f",
+    "url": "/j_portfolio/static/css/main.9bf1d1c4.chunk.css"
+  },
+  {
+    "revision": "c2f3c89a99ceb25ab01baf827099c8e0",
+    "url": "/j_portfolio/index.html"
+  }
+];
